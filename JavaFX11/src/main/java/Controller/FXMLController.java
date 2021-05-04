81c0443744aa95d44c22538ff35db05846128c72
Kiss Dániel <>
2021-05-04 19:32:51 +0200

    Azonosító valamint ennek az ellenőrző metódusának(randomNumIsIn) megírása. A delete() metódus megírása és ehhez tartozó gomb beállítása. Státusz bevezetése. A már az adatbázisban lévő adatok kiírása a megfelelő oldalakra(packageMaintenance, handleGetPackageData), azonosító bekérése után.

--- JavaFX11/src/main/java/Controller/FXMLController.java
+++ JavaFX11/src/main/java/Controller/FXMLController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import static Model.Database.delete;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +12,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import static Model.Database.intoTheTXT;
+import static Model.Database.randomNumIsIn;
 import static Model.Database.textFromtheTXT;
 import java.io.FileNotFoundException;
 import java.net.URL;
@@ -52,7 +54,7 @@ public class FXMLController {
     private Button kuldes;
     
     @FXML
-    private Label CsomagAzonosito;
+    private Text csomagAzonosito;
     
     @FXML
     private ChoiceBox<String> Statusz;
@@ -60,7 +62,33 @@ public class FXMLController {
     @FXML
     private Button karbantartas;
     
-    Random rand = new Random();
+    @FXML
+    private TextField azonGetter;
+    
+     @FXML
+    private Text packageName;
+     
+    @FXML
+    private Text packageAddr;
+
+    @FXML
+    private Text packageBillingAddr;
+
+    @FXML
+    private Text packagePhone;
+
+    @FXML
+    private Text packageComm;
+    
+    @FXML
+    private Text errorMess;
+    
+    @FXML
+    private Text errorMess2;
+    
+    @FXML
+    private Text packageStatus;
+        
     
     public void asd() {
         Statusz.getItems().removeAll(Statusz.getItems());
@@ -76,9 +104,8 @@ public class FXMLController {
             stage.setTitle("Csomag feladása");
             stage.setScene(new Scene(parent));
             stage.show();
-            //a textFromtheTXT eljáráshívás csak tesztként van benne, 
-            //majd ha lesz hely ahová meg lehet jeleníteni innen ki lehet majd venni
-            textFromtheTXT();
+            
+            
             //stage.setOnCloseRequest(e -> handleExit(false, e));
         } catch (IOException e) {
             System.out.println(e);
@@ -98,9 +125,7 @@ public class FXMLController {
 
             stage.setScene(new Scene(parent));
             stage.show();
-            //a textFromtheTXT eljáráshívás csak tesztként van benne, 
-            //majd ha lesz hely ahová meg lehet jeleníteni innen ki lehet majd venni
-            textFromtheTXT();
+            
             asd();
             //stage.setOnCloseRequest(e -> handleExit(false, e));
         } catch (IOException e) {
@@ -118,9 +143,8 @@ public class FXMLController {
             stage.setTitle("Csomag adatai");
             stage.setScene(new Scene(parent));
             stage.show();
-            //a textFromtheTXT eljáráshívás csak tesztként van benne, 
-            //majd ha lesz hely ahová meg lehet jeleníteni innen ki lehet majd venni
-            textFromtheTXT();
+            
+            
             //stage.setOnCloseRequest(e -> handleExit(false, e));
         } catch (IOException e) {
             System.out.println(e);
@@ -130,75 +154,158 @@ public class FXMLController {
     @FXML
     void backButton(ActionEvent event) {
         Stage stage = (Stage) bezar.getScene().getWindow();
-        // do what you have to do
         stage.close();
     }
 
     @FXML
     void handleGetPackageData(ActionEvent event) {
-
+        String azon = azonGetter.getText();
+        
+        String back = textFromtheTXT(azon);
+        
+        if(back.equals("error")){
+            errorMess2.setText("Nincs ilyen azonosító!");
+            errorMess2.setVisible(true);
+            packageName.setText("Címzett neve");
+            packageAddr.setText("Címzett címe");
+            packageBillingAddr.setText("Szállítási cím");
+            packagePhone.setText("Telefonszám");
+            packageComm.setText("Megjegyzés");
+            packageStatus.setText("A csomag státusza");
+        }
+        else{
+            errorMess2.setVisible(false);
+            String[] sp = back.split(",");
+            packageName.setText(sp[1]);
+            packageAddr.setText(sp[2]);
+            packageBillingAddr.setText(sp[3]);
+            packagePhone.setText(sp[4]);
+            packageComm.setText(sp[5]);
+            packageStatus.setText(sp[6]);
+        }
     }
-
+    
+    @FXML
+    void deleteButton(ActionEvent event) {
+        String azon = azonGetter.getText();
+        boolean del = delete(azon);
+        if(del){
+            errorMess2.setText("A törlés sikeres volt!");
+            errorMess2.setVisible(true);
+        }
+        else{
+            errorMess2.setText("A törlés sikertelen volt!");
+            errorMess2.setVisible(true);
+        }
+    }
+    
+    @FXML
+    void packageMaintenance(ActionEvent event) {
+        String azon = azonGetter.getText();
+        
+        String back = textFromtheTXT(azon);
+        
+        if(back.equals("error")){
+            errorMess.setVisible(true);
+            packageName.setText("Címzett neve");
+            packageAddr.setText("Címzett címe");
+            packageBillingAddr.setText("Szállítási cím");
+            packagePhone.setText("Telefonszám");
+            packageComm.setText("Megjegyzés");
+            packageStatus.setText("A csomag státusza");
+        }
+        else{
+            errorMess.setVisible(false);
+            String[] sp = back.split(",");
+            packageName.setText(sp[1]);
+            packageAddr.setText(sp[2]);
+            packageBillingAddr.setText(sp[3]);
+            packagePhone.setText(sp[4]);
+            packageComm.setText(sp[5]);
+        }
+    }
+    
     public String getDatas() {
         StringBuilder sb = new StringBuilder();
-        if (nameText.getText().isEmpty()) {
+
+        boolean bool = true;
+        int i = 0;
+        while(bool){
+            int max = 60000;
+            int min = 1;
+            int diff = max - min;
+            Random rn = new Random();
+            i = rn.nextInt(diff + 1);
+            i += min;
+            bool = randomNumIsIn(i);
+        }
+        
+        sb.append(i);
+        sb.append(",");
+        if (nameText.getText().isEmpty() || nameText.getText().isEmpty()) {
             sb.append("unknown");
         } else {
             sb.append(nameText.getText());
         }
 
         sb.append(",");
-        if (addressText.getText().isEmpty()) {
+        if (addressText.getText().isEmpty() || addressText.getText().isEmpty()) {
             sb.append("unknown");
         } else {
             sb.append(addressText.getText());
         }
 
         sb.append(",");
-        if (billingAddressText.getText().isEmpty()) {
+        if (billingAddressText.getText().isEmpty() || billingAddressText.getText().isEmpty()) {
             sb.append("unknown");
         } else {
             sb.append(billingAddressText.getText());
         }
 
         sb.append(",");
-        if (phonNumText.getText().isEmpty()) {
+        if (phonNumText.getText().isEmpty() || phonNumText.getText().isEmpty()) {
             sb.append("unknown");
         } else {
             sb.append(phonNumText.getText());
         }
 
         sb.append(",");
-        if (commentText.getText().isEmpty()) {
-            sb.append("unknown");
+        if (commentText.getText().isEmpty() || commentText.getText().isEmpty()) {
+            sb.append("Nincs");
         } else {
             sb.append(commentText.getText());
         }
+        sb.append(",Rendelés feldolgozás alatt");
 
         return sb.toString();
     }
     
+    //státuszok: rendelés feldolgozása, kiszállítás alatt, kézbesítve
     
     @FXML
     void elkuld(ActionEvent event) {
         Stage stage2 = (Stage) kuldes.getScene().getWindow();
         // do what you have to do
+        String datas = getDatas();
+        try {
+            intoTheTXT(datas);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         stage2.close();
-        
         try {
             Parent parent = FXMLLoader.load(getClass().getResource("/fxml/csomagazon.fxml"));
             Stage stage = new Stage(StageStyle.DECORATED);
             stage.setTitle("Csomag adatai");
             stage.setScene(new Scene(parent));
             stage.show();
-            //a textFromtheTXT eljáráshívás csak tesztként van benne, 
-            //majd ha lesz hely ahová meg lehet jeleníteni innen ki lehet majd venni
-            textFromtheTXT();
-            //stage.setOnCloseRequest(e -> handleExit(false, e));
+           
+            //String[] sp = datas.split(",");
+            csomagAzonosito.setText("");
         } catch (IOException e) {
             System.out.println(e);
         }
-        System.out.println(CsomagAzonosito.getText());
     }
+            
     
 }
--- JavaFX11/src/main/java/Model/Database.java
+++ JavaFX11/src/main/java/Model/Database.java
@@ -6,11 +6,15 @@
 package Model;
 
 import Controller.FXMLController;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,16 +29,37 @@ import java.util.logging.Logger;
  */
 
 class lstHelper{
+    private String name;
     private String addr;
     private String billAddr;
     private String phoneN;
     private String comm;
+    private String status;
 
-    public lstHelper(String addr, String billAddr, String phoneN, String comm) {
+    public lstHelper(String name, String addr, String billAddr, String phoneN, String comm, String status) {
+        this.name = name;
         this.addr = addr;
         this.billAddr = billAddr;
         this.phoneN = phoneN;
         this.comm = comm;
+        this.status = status;
+        
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
     
     public String getAddr() {
@@ -71,7 +96,7 @@ class lstHelper{
 
     @Override
     public String toString() {
-        return addr + ", " + billAddr + ", " + phoneN + ", " + comm;
+        return name + "," + addr + "," + billAddr + "," + phoneN + "," + comm + "," + status;
     }
     
 }
@@ -96,38 +121,101 @@ public class Database {
         }
     }
     
-    public static void textFromtheTXT(){
+    public static boolean randomNumIsIn(int randomNum){
         File op = new File("C:\\Users\\kolom\\Downloads\\K2K2\\JavaFX11\\src\\main\\java\\Model\\database.txt");
-        HashMap<String, ArrayList<lstHelper>> mp = new HashMap<>();
-        
         try {
             Scanner sc = new Scanner(op, "UTF-8");
             String[] sp;
             while(sc.hasNextLine()){
-                ArrayList<lstHelper> lst = new ArrayList<>();
                 sp = sc.nextLine().split(",");
-                lst.add(new lstHelper(sp[1], sp[2], sp[3], sp[4]));
-                if(!mp.containsKey(sp[0])){
-                    mp.put(sp[0], lst);
-                }
-                else{
-                    mp.get(sp[0]).add(new lstHelper(sp[1], sp[2], sp[3], sp[4]));
-                }
-                
-            }
-            for(var item : mp.entrySet()){
-                System.out.println(item.getKey());
-                int i = 1;
-                for(var it : item.getValue()){
-                    System.out.print("Csomag " + i + ":" + it + "\n");
-                    i++;
+                int randomNumTest = Integer.parseInt(sp[0]);
+                if(randomNumTest == randomNum){
+                    sc.close();
+                    return true;
                 }
             }
             sc.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return false;
     }
     
+    public static boolean delete(String azon){
+        File op = new File("C:\\Users\\kolom\\Downloads\\K2K2\\JavaFX11\\src\\main\\java\\Model\\database.txt");
+        ArrayList<String> lst = new ArrayList<>();
+        boolean del = true;
+        int counter = 0;
+        try {
+            Scanner sc = new Scanner(op, "UTF-8");
+            while(sc.hasNextLine()){
+                String s = sc.nextLine();
+                if(!s.contains(azon)){
+                    lst.add(s);
+                }
+                counter++;
+            }
+            sc.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        File file = new File("C:\\Users\\kolom\\Downloads\\K2K2\\JavaFX11\\src\\main\\java\\Model\\database.txt");
+        PrintWriter pw;
+        try {
+            //FileWriter fw = new FileWriter(file, Charset.forName("utf-8"), true);
+            pw = new PrintWriter(file, Charset.forName("utf-8"));
+            for(int i = 0; i < lst.size(); i++){
+                    pw.println(lst.get(i));
+            }
+            pw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if(counter == lst.size())
+            del = false;
+        else
+            del = true;
+        return del;
+    }
     
+    public static String textFromtheTXT(String azon){
+        File op = new File("C:\\Users\\kolom\\Downloads\\K2K2\\JavaFX11\\src\\main\\java\\Model\\database.txt");
+        HashMap<String, ArrayList<lstHelper>> mp = new HashMap<>();
+        
+        String back = "";
+        try {
+            Scanner sc = new Scanner(op, "UTF-8");
+            String[] sp;
+            while(sc.hasNextLine()){
+                ArrayList<lstHelper> lst = new ArrayList<>();
+                sp = sc.nextLine().split(",");
+                lst.add(new lstHelper(sp[1], sp[2], sp[3], sp[4], sp[5], sp[6]));
+                if(!mp.containsKey(sp[0])){
+                    mp.put(sp[0], lst);
+                }
+                else{
+                    mp.get(sp[0]).add(new lstHelper(sp[1], sp[2], sp[3], sp[4], sp[5], sp[6]));
+                }
+                
+            }
+            
+            for(var item : mp.entrySet()){
+                if(azon.equals(item.getKey())){
+                    back = item.getKey() + ",";
+                    for(var it : item.getValue()){
+                        back = back.concat(it.toString());
+                    }
+                }
+            }
+            
+            sc.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if(back.isBlank() || back.isEmpty()){
+            back = "error";
+        }
+        return back;
+    }
 }
